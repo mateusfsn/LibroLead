df70516531b48ae83a06ff4ddcba081f535144ea
Mateus Costa <mateusfsnfk@gmail.com>
2023-04-25 00:16:18 -0300

    First commit

---
+++ src/main/java/com/finalproject/librolead/LibroleadApplication.java
@@ -0,0 +1,13 @@
+package com.finalproject.librolead;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibroleadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibroleadApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/finalproject/librolead/LibroleadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.finalproject.librolead;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibroleadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
