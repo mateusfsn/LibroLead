c18e42a6234b4a42ae0372436074f2be637eb0f4
Mateus Costa <65982686+mateusfsn@users.noreply.github.com>
2023-05-20 11:50:33 -0300

    Added Structure Classes for Publishers Module

---
+++ src/main/java/com/finalproject/librolead/publishers/controller/PublisherController.java
@@ -0,0 +1,18 @@
+package com.finalproject.librolead.publishers.controller;
+
+import com.finalproject.librolead.publishers.service.PublisherService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/publishers")
+public class PublisherController {
+
+    private PublisherService publisherService;
+
+    @Autowired
+    public PublisherController(PublisherService publisherService) {
+        this.publisherService = publisherService;
+    }
+}
---
+++ src/main/java/com/finalproject/librolead/publishers/controller/PublisherControllerDocs.java
@@ -0,0 +1,7 @@
+package com.finalproject.librolead.publishers.controller;
+
+import io.swagger.annotations.Api;
+
+@Api("Publishers management")
+public interface PublisherControllerDocs {
+}
