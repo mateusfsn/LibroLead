9b3e56a53096eb6f2ca7343a7d34cc08da3ac7eb
Mateus Costa <65982686+mateusfsn@users.noreply.github.com>
2023-05-20 12:32:56 -0300

    Developed publisher creation functionality

--- src/main/java/com/finalproject/librolead/publishers/repository/PublisherRepository.java
+++ src/main/java/com/finalproject/librolead/publishers/repository/PublisherRepository.java
@@ -3,5 +3,9 @@ package com.finalproject.librolead.publishers.repository;
 import com.finalproject.librolead.publishers.entity.Publisher;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface PublisherRepository extends JpaRepository<Publisher, Long> {
+
+    Optional<Publisher> findByNameOrCode(String name, String code);
 }
